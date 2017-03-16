573e06c518db43db2dbfa13d972c6d41660e8a36
yothinix <m@yothinix.com>
2017-03-16 16:49:24 +0700

    map dispatch to props :rocket:

--- src/containers/TweetList.js
+++ src/containers/TweetList.js
@@ -1,8 +1,13 @@
 import { connect } from 'react-redux'
 import Tweetlist from '../components/TweetList'
+import { fetchTweetSuccess } from '../actions/tweets'
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   tweets: state.tweets,
 })
 
-export default connect(mapStateToProps)(Tweetlist)
+const mapDispatchToProps = dispatch => ({
+  fetchTweetSuccess: () => dispatch(fetchTweetSuccess()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Tweetlist)
