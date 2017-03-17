8bfb52119b43d7e90e79c6c1ced23275cceebc23
yothinix <m@yothinix.com>
2017-03-17 13:33:58 +0700

    Save token to localStorage and intialState from it :rocket:

--- src/index.js
+++ src/index.js
@@ -9,17 +9,26 @@ import { connectRouter, routerMiddleware } from 'connected-react-router'
 
 import './styles/custom.scss'
 import './styles/main.scss'
+import { loadState, saveState } from './utils/localStorage'
 import rootReducer from './reducers'
 import App from './components/App'
 
+const preloadedState = loadState()
 const history = createBrowserHistory()
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
   connectRouter(history)(rootReducer),
+  preloadedState,
   composeEnhancers(
     applyMiddleware(routerMiddleware(history), thunk),
   ),
 )
+
+store.subscribe(() => {
+  saveState({
+    auth: store.getState().auth,
+  })
+})
 
 const render = (Component) => {
   ReactDOM.render(
