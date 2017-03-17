ed902c74903a590f21be55d7ed1a99e3c9893734
yothinix <m@yothinix.com>
2017-03-17 10:59:11 +0700

    create login thunk :rocket:

---
+++ src/actions/auth.js
@@ -0,0 +1,42 @@
+import { AUTH_LOGIN_SUCCESS } from './types'
+import config from '../config'
+
+const { host, port } = config.api
+
+const loginSuccess = (username, name, token) => ({
+  type: AUTH_LOGIN_SUCCESS,
+  payload: {
+    username,
+    name,
+    token,
+  },
+})
+
+const login = (username, password) => (dispatch) => {
+  const uri = `http://${host}:${port}/api/TwisterUsers/login`
+
+  fetch(uri, {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'Conntent-Type': 'application/json',
+    },
+    mode: 'cors',
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText)
+    }
+    return response.json()
+  })
+  .then(authInfo => dispatch(loginSuccess(authInfo.username, authInfo.name, authInfo.token)))
+  .catch(err => console.error(err))
+}
+
+export {
+  login,
+}
