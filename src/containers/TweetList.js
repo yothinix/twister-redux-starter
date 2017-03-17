fd0b06367a5cde2f3025a0ef5631a022e9516cdd
yothinix <m@yothinix.com>
2017-03-17 10:04:54 +0700

    map router state to props :rocket:

--- src/containers/TweetList.js
+++ src/containers/TweetList.js
@@ -1,10 +1,19 @@
 import { connect } from 'react-redux'
+import { matchPath } from 'react-router'
 import Tweetlist from '../components/TweetList'
 import { fetchTweets } from '../actions/tweets'
 
-const mapStateToProps = state => ({
-  tweets: state.tweets,
-})
+const mapStateToProps = (state) => {
+  const { params } = matchPath(
+    state.router.location.pathnae,
+    { path: '/:ownerUsername' },
+  )
+
+  return {
+    tweets: state.tweets,
+    ownerUsername: params.ownerUsername,
+  }
+}
 
 const mapDispatchToProps = dispatch => ({
   fetchTweets: username => dispatch(fetchTweets(username)),
