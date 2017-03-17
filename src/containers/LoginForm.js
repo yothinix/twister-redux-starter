d249ad90e583ad33f621efab86d70db474e878b1
yothinix <m@yothinix.com>
2017-03-17 13:01:25 +0700

    Update loginForm to class :rocket:

--- src/containers/LoginForm.js
+++ src/containers/LoginForm.js
@@ -3,29 +3,36 @@ import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { login } from '../actions/auth'
 
-const LoginForm = ({ handleSubmit, login }) => {
-  const submitLogin = (values) => {
-    login(values.username, values.password)
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.submitLogin = this.submitLogin.bind(this)
   }
 
-  return (
-    <div className="login-form">
-      <form onSubmit={handleSubmit(submitLogin)}>
-        <div className="login-label">
-          Log in with your username
+  submitLogin(values) {
+    this.props.login(values.username, values.password)
+  }
+
+  render() {
+    return (
+      <div className="login-form">
+        <form onSubmit={this.props.handleSubmit(this.submitLogin)}>
+          <div className="login-label">
+            Log in with your username
       </div>
-        <div className="form-group">
-          <Field name="username" component="input" type="text" className="form-control" placeholder="Username" />
-        </div>
-        <div className="form-group">
-          <Field name="password" component="input" type="password" className="form-control" placeholder="Password" />
-        </div>
-        <div className="form-group text-right">
-          <button className="btn btn-default">Log in</button>
-        </div>
-      </form>
-    </div>
-  )
+          <div className="form-group">
+            <Field name="username" component="input" type="text" className="form-control" placeholder="Username" />
+          </div>
+          <div className="form-group">
+            <Field name="password" component="input" type="password" className="form-control" placeholder="Password" />
+          </div>
+          <div className="form-group text-right">
+            <button className="btn btn-default">Log in</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
 LoginForm.propTypes = {
